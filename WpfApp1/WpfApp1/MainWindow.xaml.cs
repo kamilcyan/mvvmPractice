3472f49eed579dd35f8885336329da8996aeaf0f
kamilcyan <kamil.lucjanek@wp.pl>
2020-02-16 02:00:58 +0100

    x

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using KoloryWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Color kolor = Ustawienia.Czytaj();
+            rectangle.Fill = new SolidColorBrush(kolor);
+            sliderR.Value = kolor.R;
+            sliderG.Value = kolor.G;
+            sliderB.Value = kolor.B;
+        }
+
+        private void SliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Color color = Color.FromRgb(
+                (byte)sliderR.Value,
+                (byte)sliderG.Value,
+                (byte)sliderB.Value
+                );
+            (rectangle.Fill as SolidColorBrush).Color = color;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                Close();
+        }
+
+        private Color KolorProstokata
+        {
+            get
+            {
+                return (rectangle.Fill as SolidColorBrush).Color;
+            }
+            set
+            {
+                (rectangle.Fill as SolidColorBrush).Color = value;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Ustawienia.Zapisz(KolorProstokata);
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Ustawienia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace KoloryWPF
+{
+    static class Ustawienia
+    {
+        public static Color Czytaj()
+        {
+            WpfApp1.Properties.Settings ustawienia = WpfApp1.Properties.Settings.Default;
+            Color kolor = new Color()
+            {
+                A = 255,
+                R = ustawienia.R,
+                G = ustawienia.G,
+                B = ustawienia.B
+            };
+            return kolor;
+        }
+        public static void Zapisz(Color kolor)
+        {
+            WpfApp1.Properties.Settings ustawienia = WpfApp1.Properties.Settings.Default;
+            ustawienia.R = kolor.R;
+            ustawienia.G = kolor.G;
+            ustawienia.B = kolor.B;
+            ustawienia.Save();
+        }
+    }
+}
